ab12ca4003b3c00c5ad0a0f48b2d33cc0121b973
Maxim Sylka <msylka@me.com>
2024-01-16 23:02:58 +0300

    Initial Commit

---
+++ TabBarApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabBarApp
+//
+//  Created by Максим Сылка on 16.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
