a0fb7b5158e09fd1ab04deb1f7297de9393e615c
Maxim Sylka <msylka@me.com>
2024-01-18 00:25:53 +0300

    Embedded ViewController to UInavigationController

--- TabBarApp/MainViewController.swift
+++ TabBarApp/MainViewController.swift
@@ -7,19 +7,23 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
+final class MainViewController: UIViewController {
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let tabBarVC = segue.destination as ? UITabBarController
-        tabBarVC?.viewControllers?.forEach {
-            VC in
-            if let firstVC = vc as? FirstViewController {
+        let tabBarVC = segue.destination as? UITabBarController
+        
+        tabBarVC?.viewControllers?.forEach { viewController in
+            if let firstVC = viewController as? FirstViewController {
                 firstVC.view.backgroundColor = .systemOrange
             } else if let secondVC = viewController as? SecondViewController {
                 secondVC.view.backgroundColor = .systemMint
+            } else if let navigationVC = viewController as? UINavigationController {
+        let thirdVC = navigationVC.topViewController as? ThirdViewController
+                    thirdVC?.title = "Third Screen"
+                    thirdVC?.view.backgroundColor = .systemCyan
+                }
             }
         }
     }
 
-}
 
